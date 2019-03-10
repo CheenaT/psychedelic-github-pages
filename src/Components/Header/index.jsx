c4bf6c78f501ea9b602786d42d1c58f34009c417
CheenaT <chingiz.tursunaliyev@gmail.com>
2019-03-10 15:27:14 +0300

    Finished user component and testing @media

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { ReactComponent as Exit__Icon } from '../../img/icon-exit.svg';
 
 const Header = () => (
-  <div className = "bg">
+  <div className = "Header">
     <nav>
       <ul>
         <li>Связаться с банком</li>
         <li>Офисы и банкоматы</li>
+        <li className = "icon__exit"><Exit__Icon /></li>
         <li>Выйти</li>
       </ul>
     </nav>
--- src/Components/User/index.jsx
+++ src/Components/User/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from '../../img/user__icon.jpg';
+import { ReactComponent as Settings__Icon } from '../../img/settings-24.svg'
 
 class User extends React.Component {
   render() {
@@ -8,6 +9,13 @@ class User extends React.Component {
         <div className="user__icon">
           <img src={Image} alt="user icon"/>
         </div>
+        <div className="user__greetings">
+          Здравствуйте,
+        </div>
+        <div className="user__name">
+          Алиса
+        </div>
+        <Settings__Icon className="user__settings__icon"/>
       </div>
     );
   }
